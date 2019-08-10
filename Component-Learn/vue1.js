931abb2d6881c1f2a7184df335b770da769951cb
yu-kitano <ktn.ysk.1996@gmail.com>
2019-08-10 20:42:42 +0900

    減少ボタンの追加。

--- Component-Learn/vue1.js
+++ Component-Learn/vue1.js
@@ -1,33 +1,52 @@
-var counterButton = Vue.extend({
-  template: '<span>{{counter}}個<button v-on:click="addToCart">追加</button></span>',
-  data: function () {
-    return {
-      counter: 0
-    }
-  },
+var incrementButton = Vue.extend({
+  template: '<span><button v-on:click="addToCart">追加</button></span>',
   methods: {
     addToCart: function () {
-      this.counter += 1
       this.$emit('increment') // incrementカスタムイベントの発火
     }
   },
 })
+// 数量を減らす用のコンポーネント
+var decrementButton = Vue.extend({
+  template: '<span><button v-on:click="subToCart">減少</button></span>',
+  methods: {
+    subToCart: function () {
+    	// htmlコンポーネントのv-on:decrementで指定している関数を呼び出す
+      this.$emit('decrement') 
+    }
+  },
+})
 
+// fruit-counterブロックにマウントするためのvueコンストラクタ
 new Vue({
   el: '#fruits-counter',
   components:{
-    'counter-button': counterButton
+    'increment-button': incrementButton,
+    'decrement-button': decrementButton,    
   },
   data: {
     total: 0,
     fruits: [
-      {name: '梨'},
-      {name: 'イチゴ'}
+      {name: '梨', sum: 0 },
+      {name: 'イチゴ', sum: 0}
     ]
   },
   methods: {
-    incrementCartStatus: function () {
+    incrementCartStatus: function (name,sum) {
+    	if(name=="梨"){
+      	this.fruits[0].sum = sum
+      } else {
+      	this.fruits[1].sum = sum
+      }
       this.total += 1
+    },
+    decrementCartStatus: function (name,sum) {
+    	if(name=="梨"){
+      	this.fruits[0].sum = sum
+      } else {
+      	this.fruits[1].sum = sum
+      }
+      this.total -= 1
     }
   }
 })
