62e1088a7c1fa5b6effe57fb4779b15d0a496426
yu-kitano <ktn.ysk.1996@gmail.com>
2020-04-12 19:48:49 +0900

    単一コンポーネントについて学習

---
+++ SingleFileComponent/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1 class="red">あいさつ：{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data () {
+    return {msg: "hello Vue"}
+  }
+}
+</script>
+
+<style scoped>
+.red {
+  color: red;
+}
+</style>
